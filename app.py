a8c2f066e52eeeaaaa4674d840cac097ac938827
Sharathbabugv1299 <sharathbabu1299@gmail.com>
2024-09-17 20:44:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ def upload_image():
     
     return render_template('upload.html')
 
-# Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
